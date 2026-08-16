cfbc5360dd085605cd3572c3740eef8601601050
danyywhite <sultanbekovdanil@gmail.com>
2026-08-17 01:55:17 +0300

    завершил

---
+++ prt.js
@@ -0,0 +1,16 @@
+/*домашняя работа*/
+let name = "Alex";
+let age = 20;
+let hasAccount = true;
+
+console.log(`Привет, ${name}!`);
+if ( age >= 18) {
+    console.log("Совершеннолетний");
+} else {
+    console.log("Доступ запрещен");
+}
+if (hasAccount === true) {
+    console.log("Аккаунт имеется");
+}
+let arrayFsd = [apple, orange, lime];
+arrayFsd.push()
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -3,3 +3,11 @@ let age = 20;
 let hasAccount = true;
 
 console.log(`Привет, ${name}!`);
+if ( age >= 18) {
+    console.log("Совершеннолетний");
+} else {
+    console.log("Доступ запрещен");
+}
+if (hasAccount === true) {
+    console.log("Аккаунт имеется");
+}
