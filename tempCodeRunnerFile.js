374abbc7dd1b2065b804ee3456d2a7f8ccbb5e3b
danyywhite <sultanbekovdanil@gmail.com>
2026-08-20 16:47:24 +0300

    functiom

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,17 +1,9 @@
-let name = "Alex";
-let age = 20;
-let hasAccount = true;
-
-console.log(`Привет, ${name}!`);
-if ( age >= 18 && hasAccount === true) {
-    console.log("Доступ разрешен");
-} else {
-    console.log("Доступ запрещен");
+function checkPassword(password){
+    if (password === "12345"){
+        return "Заходи бро";
+    } else {
+        return "Развернись и зайди нормально";
+    }
 }
-let fruty = ["apple","mango","lime" ];
-fruty.push("limon");
-console.log(fruty);
-
-for (let i = 0; i < fruty.length; i++){
-    console.log(fruty[i]);
-}
+let result = checkPassword("234");
+console.log(result);
