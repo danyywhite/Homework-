a9e3f251f063669efbfb335b45a77c67349393e0
danyywhite <sultanbekovdanil@gmail.com>
2026-08-20 14:20:38 +0300

    new comm

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -3,11 +3,15 @@ let age = 20;
 let hasAccount = true;
 
 console.log(`Привет, ${name}!`);
-if ( age >= 18) {
-    console.log("Совершеннолетний");
+if ( age >= 18 && hasAccount === true) {
+    console.log("Доступ разрешен");
 } else {
     console.log("Доступ запрещен");
 }
-if (hasAccount === true) {
-    console.log("Аккаунт имеется");
+let fruty = ["apple","mango","lime" ];
+fruty.push("limon");
+console.log(fruty);
+
+for (let i = 0; i < 4; i++){
+    console.log(fruty[i]);
 }
